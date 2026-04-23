6c1dc92e3c6676072a633fad4f4eca154988e1c6
seyeongpark <lynn.yeong.102@gmail.com>
2026-04-23 23:35:59 +0900

    test: add login scenarios with data-driven cases

---
+++ tests/ui/test_login.py
@@ -0,0 +1,36 @@
+# tests/test_login.py
+
+import pytest, json
+from pages.login_page import LoginPage
+from playwright.sync_api import expect
+from pathlib import Path
+
+DATA_FILE = Path(__file__).parent.parent.parent / "data" / "login.json"
+
+with open(DATA_FILE, encoding="utf-8") as f:
+  login_cases = json.load(f)
+
+LOGIN_CASES = [
+    (
+        case["user_id"],
+        case["username"],
+        case["password"],
+        case["expected_error"],
+        case["expected_url"]
+    )
+    for case in login_cases
+]
+
+@pytest.mark.parametrize("user_id, username, password, expected_msg, expected_url", LOGIN_CASES)
+def test_login(page, base_url, user_id, username, password, expected_msg, expected_url):
+  login_page = LoginPage(page)
+  login_page.open(base_url)
+  print(f"[TEST CASE] {user_id}")
+  login_page.login(username, password)
+
+  if expected_msg:
+    expect(login_page.text_error).to_contain_text(expected_msg)
+    expect(login_page.page).to_have_url(expected_url)
+  else:
+    expect(login_page.text_error).to_be_hidden()
+    expect(login_page.btn_hamburger).to_be_visible()
